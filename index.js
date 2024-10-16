da97b4ad7f418714667ea590c49095d71db83085
Rere243-fullstack <rethambombo@gmail.com>
2024-10-16 20:02:29 +0100

    defintion de la structure de controle

--- index.js
+++ index.js
@@ -6,16 +6,34 @@
 // let age1=20
 // let age2=12
 // const dateAnniversaire=12
-let number1=12;
-let number2=10
-let resultat=0
-let resultat1=0
-   resultat= number1 + number2
+// let number1=12;
+// let number2=10
+// let resultat=0
+// let resultat1=0
+//    resultat= number1 + number2
 
-   resultat1=number1-number2
-console.log("Addition de deux nombre"+resultat,resultat1)
-console.log(`Addition ${resultat}, ${resultat1}`)
+//    resultat1=number1-number2
+// console.log("Addition de deux nombre"+resultat,resultat1)
+// console.log(`Addition ${resultat}, ${resultat1}`)
+let nombre1=6
+resultat= 6%4
+console.log(resultat)
+// if "instrinction1" else "instruction2"
+const age=15
+let nom="Kabasele"
+if(age>=18){
+    console.log("vous êtes adult"+ age)
+}else{
+    console.log("vous êtes mineur"+ age)
+}
+
+if(nom == "Mbombo"){
+    console.log(`${nom} appartient à la famille Mbombo`)
+}else{
+    console.log()
+    console.log(`${nom} n'appartient pas à la famille Mbombo`)
+}
 
 
-console.log(nomPostnom)
-console.log(age,age1,age2)
+// console.log(nomPostnom)
+// console.log(age,age1,age2)
