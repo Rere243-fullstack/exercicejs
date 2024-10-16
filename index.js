f83b8a7c939985fffcdc3ca7d53614b0c2e87bb0
Rere243-fullstack <rethambombo@gmail.com>
2024-10-16 18:07:22 +0100

    premier commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//var ,let,const 
+let name="retha"
+let name1="patrick"
+let nomPostnom="camelcase"
+let age=12
+let age1=20
+let age2=12
+
+console.log(nomPostnom)
+console.log(age,age1,age2)
---
+++ readme.md
@@ -0,0 +1,5 @@
+comment declarer une variable e javascript
+comment affecter une valeur à une variable
+comment realiser une operation en javascript
+//initialisation du dossier
+git init
